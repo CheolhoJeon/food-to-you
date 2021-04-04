89404e0f1a97858e365eb30b6620f87a457d0444
전철호 <cheolho@wisoft.io>
2021-04-04 16:31:31 +0900

    그레이들 기반의 스프링 설정

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # food-to-you
+
 다양한 기술 시도를 위한 토이 프로젝트입니다.
 
 # Target Technique
+
 * Kotlin
 * Spring Framework
 * JPA
---
+++ src/main/java/com/charlie/fty/FootToYouApplication.java
@@ -0,0 +1,13 @@
+package com.charlie.fty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootToYouApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FootToYouApplication.class, args);
+  }
+
+}
